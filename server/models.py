00f44b5b4fa41086fd727b863c907e109dbc3f0f
Jeffrey <j.lo128456@gmail.com>
2025-04-22 19:15:00 +1000

    completed lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,23 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("Author must have a name.")
+
+        # Check if another author with the same name already exists
+        existing = Author.query.filter_by(name=value).first()
+        if existing:
+            raise ValueError(f"Author with name '{value}' already exists.")
+        
+        return value
+
+    @validates('phone_number')
+    def validate_phone_number(slef, key, value):
+        if not value.isdigit() or len(value) !=10:
+            raise ValueError("Phone number must be exactly 10 digits.") 
+        return value           
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +44,38 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators
+    @validates('title')
+    def validate_title(self, key, value):
+        if not value:
+            raise ValueError("Post must have a title.")
+
+        clickbait_phrases = [
+            "Won't Believe", "Secret", "Top", "Guess"
+        ]
+
+        if not any(phrase in value for phrase in clickbait_phrases):
+            raise ValueError("Title must be clickbait.")
+        
+        return value
+
+    @validates('content')
+    def validate_content(self, key, value):
+        if not value or len(value) < 250:
+            raise ValueError("Post content must be at least 250 characters.")
+        return value
+
+    @validates('summary')
+    def validate_summary(self, key, value):
+        if len(value) > 250:
+            raise ValueError("Summary must be at most 250 characters.")
+        return value
+
+    @validates('category')
+    def validate_category(self, key, value):
+        if value not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'.")
+        return value  
 
 
     def __repr__(self):
